71e185cf70bc238ded0550c3e9702732b133de08
Karan Sikka <kssworld93@gmail.com>
2014-01-28 16:56:06 -0500

    switched 1 and 0

--- bin/is_not_running.py
+++ bin/is_not_running.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-## EXITS 0 IF DOCKER CONTAINER IS RUNNING, 1 IF NOT RUNNING.
+## EXITS 1 IF DOCKER CONTAINER IS RUNNING, 0 IF NOT RUNNING.
 
 
 import json
@@ -8,6 +8,6 @@ import sys
 
 container_data = json.loads(sys.stdin.read())
 if container_data[0]['State']['Running']:
-    sys.exit(0)
-else:
     sys.exit(1)
+else:
+    sys.exit(0)
